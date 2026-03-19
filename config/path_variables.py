80566929e50a9348297bd12fa074c8f10dd15625
topcue <topcue1019@gmail.com>
2026-03-19 14:06:11 +0900

    Fix config

--- config/path_variables.py
+++ config/path_variables.py
@@ -1,5 +1,4 @@
-IDA_PATH="/home/dongkwan/.tools/ida-7.6"
-IDA_FETCH_FUNCDATA="/home/dongkwan/tiknib/tiknib/ida/fetch_funcdata_v7.5.py"
+IDA_PATH="C:/Users/user/workspace/IDA"
+IDA_FETCH_FUNCDATA="C:/Users/user/workspace/IDA/tiknib/ida/fetch_funcdata_v7.5.py"
 
-TIKNIB_REPO="/home/dongkwan/tiknib"
-BINKIT_DATASET="/home/dongkwan/binkit-dataset"
+BINKIT_DATASET="C:/Users/user/workspace/storage/dataset"
